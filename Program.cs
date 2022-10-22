38af40f7ae78fa9d28a95aeb4584de3b27a28275
Bianca <biancabatinas64@gmail.com>
2022-10-22 21:02:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batinas3132B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
